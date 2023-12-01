82010b8849baaea7c1a79aa63201afdcd4451f31
Alexandre Clenet <alexandreclenet@free.fr>
2023-12-01 16:59:39 +0100

    test data

--- NaviGuesser/data.py
+++ NaviGuesser/data.py
@@ -1,12 +1,11 @@
-
 import pandas as pd
+pd.set_option('display.max_columns', 14)
+pd.set_option('display.width', 1000)
 # Loading the necessary files
 routes_df = pd.read_csv('NaviGuesser/gtfs-tan/routes.txt')
 trips_df = pd.read_csv('NaviGuesser/gtfs-tan/trips.txt')
-stop_times_df = pd.read_csv('NaviGuesser/gtfs-tan/stop_times.txt',low_memory=False)
+stop_times_df = pd.read_csv('NaviGuesser/gtfs-tan/stop_times.txt', low_memory=False)
 stops_df = pd.read_csv('NaviGuesser/gtfs-tan/stops.txt')
-
-#print(routes_df)
 
 # Merging the data to get the route for each stop
 # First, merge trips with routes
@@ -14,23 +13,21 @@ route_trip_df = pd.merge(trips_df[['route_id', 'trip_id']], routes_df[['route_id
 # Then, merge with stop_times
 route_trip_stop_df = pd.merge(route_trip_df, stop_times_df[['trip_id', 'stop_id']], on='trip_id')
 # Finally, merge with stops
-route_stop_df = pd.merge(route_trip_stop_df, stops_df[['stop_id', 'stop_name']], on='stop_id')
+route_stop_df = pd.merge(route_trip_stop_df, stops_df, on='stop_id')
 
 # Removing duplicates and sorting
 route_stop_df = route_stop_df.drop_duplicates(subset=['route_id', 'stop_id']).sort_values(by=['route_id', 'stop_id'])
 
-# Preview the resulting DataFrame
-route_stop_df.head()
-# To get the stops in the order of passage for route 4, we need to look at a specific trip
-# Let's find a trip for route 4 and then list the stops in the order they are visited in that trip
+def getRouteWithStop(idRoute):
+    # Filtering for the specified route
+    route_stops = route_stop_df[route_stop_df['route_short_name'] == idRoute].sort_values(by=['trip_id', 'stop_id'])
+    
+    # Dropping duplicates and resetting index
+    unique_stops_route = route_stops.drop_duplicates(subset=['stop_id']).reset_index(drop=True)
+    
+    return unique_stops_route
 
-# First, get a trip ID for route 4
-trip_id_for_route_4 = route_stop_df[route_stop_df['route_short_name'] == '4']['trip_id'].iloc[0]
-# Now, filter stop_times_df for this trip ID and order by the stop sequence
-stops_in_order_for_route_4 = stop_times_df[stop_times_df['trip_id'] == trip_id_for_route_4].sort_values(by='stop_sequence')
-# Merging with stops to get stop names
-stops_in_order_for_route_4 = pd.merge(stops_in_order_for_route_4, stops_df[['stop_id', 'stop_name']], on='stop_id')
-# Selecting relevant columns
-stops_in_order_for_route_4 = stops_in_order_for_route_4[['stop_sequence', 'stop_name']].reset_index(drop=True)
-
-print(stops_in_order_for_route_4)
+# Display all stop information for route 27
+route_27_stops_info = getRouteWithStop('27')
+#print(route_27_stops_info)
+print(routes_df)
--- NaviGuesser/src/dao/GTFS.ts
+++ NaviGuesser/src/dao/GTFS.ts
@@ -1,57 +1,13 @@
-import fs from 'fs';
-import csv from 'csv-parser';
+/*import { openDb } from 'gtfs';
+import { readFile } from 'fs/promises';
+const config = JSON.parse(
+  await readFile(new URL('./config.json', import.meta.url))
+);
+const db = openDb(config);
 
-const GTFS_FILES_PATH = 'NaviGuesser/gtfs-tan';
-
-function readCsvFile<T>(filePath: string): Promise<T[]> {
-    return new Promise((resolve, reject) => {
-        const results: T[] = [];
-        fs.createReadStream(filePath)
-            .pipe(csv())
-            .on('data', (data) => results.push(data))
-            .on('end', () => resolve(results))
-            .on('error', (error) => reject(error));
-    });
-}
-
-export async function getTripsForRoute(routeId: string) {
-    const trips = await readCsvFile<any>(`${GTFS_FILES_PATH}/trips.txt`);
-    return trips.filter(trip => trip.route_id === routeId);
-}
-
-export async function getStopTimesForTrip(tripId: string) {
-    const stopTimes = await readCsvFile<any>(`${GTFS_FILES_PATH}/stop_times.txt`);
-    return stopTimes.filter(stopTime => stopTime.trip_id === tripId);
-}
-
-export async function getStop(stopId: string) {
-    const stops = await readCsvFile<any>(`${GTFS_FILES_PATH}/stops.txt`);
-    return stops.find(stop => stop.stop_id === stopId);
-}
-
-export async function getAllStop() {
-    const stops = await readCsvFile<any>(`${GTFS_FILES_PATH}/stops.txt`);
-    return stops
-}
-
-export async function getStopsInOrderForRoute(routeId: string) {
-    const trips = await getTripsForRoute(routeId);
-    const firstTrip = trips[0]; // Choix d'un trip spécifique
-    const stopTimes = await getStopTimesForTrip(firstTrip.trip_id);
-    stopTimes.sort((a, b) => a.stop_sequence - b.stop_sequence);
-
-    const stopsInOrder = [];
-    for (const stopTime of stopTimes) {
-        const stop = await getStop(stopTime.stop_id);
-        stopsInOrder.push(stop);
-    }
-
-    return stopsInOrder;
-}
-
-// Exemple d'utilisation
-const routeId = '4'; // ID de la route souhaitée
-getStopsInOrderForRoute(routeId).then(stops => {
-    console.log('Stops in order for route:', routeId);
-    stops.forEach(stop => console.log(stop.stop_name));
-});
+const routes = getRoutes(
+    {}, // No query filters
+    ['route_id', 'route_short_name', 'route_color'], // Only return these fields
+    [['route_short_name', 'ASC']], // Sort by this field and direction
+    { db: db } // Options for the query. Can specify which database to use if more than one are open
+  );*/
