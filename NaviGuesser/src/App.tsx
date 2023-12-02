bb4885d642ee2a47e503de6a419828dfa9f383b9
E214194U <alexandreclenet@free.fr>
2023-12-02 01:23:02 +0100

    route by type

--- NaviGuesser/src/App.tsx
+++ NaviGuesser/src/App.tsx
@@ -1,24 +1,45 @@
 import "./App.css";
-import { getAllRoute, getAllStop } from "./dao/GTFS";
+import SVGbyRouteType from "./component/SVGbyRouteType";
+import { getAllRoute, getAllRoutebyType, getAllStop } from "./dao/GTFS";
+
+interface Route {
+  route_id: string;
+  route_type: number;
+  route_text_color: string;
+  route_color: string;
+  route_short_name: string;
+  // ... other properties
+}
 
 function App() {
   const routes = getAllRoute();
   const stops = getAllStop();
-
-  const routesList = routes.map((item: any) => (
-    <li key={item.route_id}>
-      <div
-        className="relative flex flex-col items-center justify-center shadow-sm group w-6 h-6 text-[10px] font-bold"
-        style={{
-          color: "#" + item.route_text_color,
-          backgroundColor: "#" + item.route_color,
-        }}
-      >
-        <div className="leading-none">{item.route_short_name}</div>
-        <div className="absolute inset-0 transition-opacity duration-500 group-hover:opacity-0"></div>
+  
+  // Assuming routesTyped is an array of [route_type, Route[]] pairs
+  const routesTyped: [string, Route[]][] = Object.entries(getAllRoutebyType());
+  
+  const routesList = routesTyped.map(([routeType  , routes]) => {
+    const laneList = routes.map((route:any) => (
+      <li key={route.route_id}>
+        <div
+          className="relative flex flex-col items-center justify-center shadow-sm group w-6 h-6 text-[10px] font-bold"
+          style={{
+            color: "#" + route.route_text_color,
+            backgroundColor: "#" + route.route_color,
+          }}
+        >
+          <div className="leading-none">{route.route_short_name}</div>
+          <div className="absolute inset-0 transition-opacity duration-500 group-hover:opacity-0"></div>
+        </div>
+      </li>
+    ));
+    return (
+      <div className="flex items-start gap-4 py-2">
+        {SVGbyRouteType(Number(routeType))}
+        <ul className="flex flex-wrap items-center gap-2">{laneList}</ul>
       </div>
-    </li>
-  ));
+    );
+  });
   return (
     <>
       <div className="flex flex-col items-stretch w-screen h-full justify-stretch lg:flex-row">
@@ -31,15 +52,12 @@ function App() {
               <div className="text-sm font-medium uppercase text-slate-800">
                 {routes.length} lignes à Nantes
               </div>
-              <ul className="flex flex-wrap items-center gap-2">
-                {routesList}
-              </ul>
+              {routesList}
             </div>
             <div className="flex flex-col p-4 space-y-4 bg-white rounded-lg grow">
               <div className="text-sm font-medium uppercase text-slate-800">
-                Aucun arrêt trouvé sur {stops.length} 
+                Aucun arrêt trouvé sur {stops.length}
               </div>
-
             </div>
           </div>
         </div>
--- NaviGuesser/src/dao/GTFS.ts
+++ NaviGuesser/src/dao/GTFS.ts
@@ -1,20 +1,37 @@
 import stop from './parent_stops_with_routes.json';
 import route from './routes.json';
 
-export function getAllRoute(){   
+
+
+export function getAllRoute() {   
   try {
-    const medicaments=JSON.parse(JSON.stringify(route))
-    return medicaments
+    return JSON.parse(JSON.stringify(route)) ;
   } catch (error) {
     console.error('Error reading JSON file', error);
   }
 }
 
-export function getAllStop(){   
+export function getAllStop() {   
   try {
-    const medicaments=JSON.parse(JSON.stringify(stop))
-    return medicaments
+    return JSON.parse(JSON.stringify(stop))
   } catch (error) {
     console.error('Error reading JSON file', error);
+  }
+}
+
+export function getAllRoutebyType() {
+  try {
+    const routes = JSON.parse(JSON.stringify(route));
+
+    return routes.reduce((acc: { [x: string]: any[]; }, currentRoute: { route_type: string | number; }) => {
+      // Initialize an array for the route_type if it doesn't exist
+      acc[currentRoute.route_type] = acc[currentRoute.route_type] || [];
+      // Add the current route to its route_type array
+      acc[currentRoute.route_type].push(currentRoute);
+
+      return acc;
+    }, {} );
+  } catch (error) {
+    console.error('Error processing JSON file', error);
   }
 }
